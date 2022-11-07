d83f6224101e03ff2d156ee6d149f076b539e345
Lahiru Rajapaksha <lahiruzone@gmail.com>
2022-11-07 15:45:13 +0530

    relase(web): v1.0.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "15ee2c0ad16a471e72a0d858c1ae4ffa",
-"index.html": "d1888699f794b925cf3bd53551375d09",
-"/": "d1888699f794b925cf3bd53551375d09",
-"main.dart.js": "85dfe60195148703d7faf1b3c1e0e75d",
+"index.html": "2a09dc36103393b411cce8c9222a2940",
+"/": "2a09dc36103393b411cce8c9222a2940",
+"main.dart.js": "1d57b45c757aad9aae3d07b56cc67650",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
